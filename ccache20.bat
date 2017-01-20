6543697eb2bd4c00e59e395eab7ce8e2c2489172
DVX opensource projects <reunisoft@gmail.com>
2017-01-20 12:50:08 +0100

    Create ccache20.bat
    This script will clean up your computer, and also Firefox, Internet Explorer and Google Chrome.

---
+++ ccache20.bat
@@ -0,0 +1,76 @@
+
+    @REM FROM MYSELF
+
+    taskkill /F /IM firefox.exe
+    taskkill /F /IM iexplore.exe
+    taskkill /F /IM chrome.exe
+
+    @REM FROM STACKOVERFLOW https://stackoverflow.com/questions/12621969/clear-cache-of-browser-by-command-line
+
+    erase "%TEMP%\*.*" /f /s /q
+    for /D %%i in ("%TEMP%\*") do RD /S /Q "%%i"
+
+    erase "%TMP%\*.*" /f /s /q
+    for /D %%i in ("%TMP%\*") do RD /S /Q "%%i"
+
+    erase "%ALLUSERSPROFILE%\TEMP\*.*" /f /s /q
+    for /D %%i in ("%ALLUSERSPROFILE%\TEMP\*") do RD /S /Q "%%i"
+
+    erase "%SystemRoot%\TEMP\*.*" /f /s /q
+    for /D %%i in ("%SystemRoot%\TEMP\*") do RD /S /Q "%%i"
+
+    @rem Clear IE cache - (Deletes Temporary Internet Files Only)
+    RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+    erase "%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*.*" /f /s /q
+    for /D %%i in ("%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*") do RD /S /Q "%%i"
+
+    @rem Clear Google Chrome cache
+    erase "%LOCALAPPDATA%\Google\Chrome\User Data\*.*" /f /s /q
+    for /D %%i in ("%LOCALAPPDATA%\Google\Chrome\User Data\*") do RD /S /Q "%%i"
+
+    @rem Clear Firefox cache
+    erase "%LOCALAPPDATA%\Mozilla\Firefox\Profiles\*.*" /f /s /q
+    for /D %%i in ("%LOCALAPPDATA%\Mozilla\Firefox\Profiles\*") do RD /S /Q "%%i"
+
+    @FROM STACKOVERFLOW http://www.catonmat.net/blog/clear-privacy-ie-firefox-opera-chrome-safari/
+
+    set ChromeDir=C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data
+
+    del /q /s /f "%ChromeDir%"
+    rd /s /q "%ChromeDir%"
+
+    set DataDir=C:\Users\%USERNAME%\AppData\Local\Mozilla\Firefox\Profiles
+
+    del /q /s /f "%DataDir%"
+    rd /s /q "%DataDir%"
+
+    for /d %%x in (C:\Users\%USERNAME%\AppData\Roaming\Mozilla\Firefox\Profiles\*) do del /q /s /f %%x\*sqlite
+
+    set DataDir=C:\Users\%USERNAME%\AppData\Local\Opera\Opera
+    set DataDir2=C:\Users\%USERNAME%\AppData\Roaming\Opera\Opera
+
+    del /q /s /f "%DataDir%"
+    rd /s /q "%DataDir%"
+
+    del /q /s /f "%DataDir2%"
+    rd /s /q "%DataDir2%"
+
+    set DataDir=C:\Users\%USERNAME%\AppData\Local\Applec~1\Safari
+    set DataDir2=C:\Users\%USERNAME%\AppData\Roaming\Applec~1\Safari
+
+    del /q /s /f "%DataDir%\History"
+    rd /s /q "%DataDir%\History"
+
+    del /q /s /f "%DataDir%\Cache.db"
+    del /q /s /f "%DataDir%\WebpageIcons.db"
+
+    del /q /s /f "%DataDir2%"
+    rd /s /q "%DataDir2%"
+
+    set FlashCookies=C:\Users\%USERNAME%\AppData\Roaming\Macromedia\Flashp~1
+
+    del /q /s /f "%FlashCookies%"
+    rd /s /q "%FlashCookies%"
+
+
+    pause
