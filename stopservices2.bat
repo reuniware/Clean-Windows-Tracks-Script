3853c0d8bbfee244c372c5dd40f5f0ef21decf8d
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2018-11-22 11:53:57 +0100

    Create stopservices2.bat

---
+++ stopservices2.bat
@@ -0,0 +1,16 @@
+net stop stisvc
+net stop hidserv
+net stop AdobeARMservice
+net stop "Microsoft IIS Administration"
+net stop ss_conn_service
+net stop W3SVC
+net stop CryptSvc
+net stop WinHttpAutoProxySvc
+net stop TeamViewer
+net stop Spooler
+net stop WSearch
+net stop wuauserv
+net stop BITS
+net stop SSDPSRV
+net stop SQLWriter
+net stop CscService
