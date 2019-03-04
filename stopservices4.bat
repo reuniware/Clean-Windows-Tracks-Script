47c56822edba6f25e90c4b443d72e8c3738f3f40
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2019-03-04 08:48:18 +0100

    Create stopservices4.bat

---
+++ stopservices4.bat
@@ -0,0 +1,76 @@
+rem Stop a maximum number of Windows Services to speed-up the system
+sc stop wscsvc
+sc stop WSearch
+sc stop AdobeARMservice
+sc stop AeLookupSvc
+rem Stopping Appinfo disable possibility to launch any program as Administrator
+rem sc stop Appinfo
+sc stop BFE
+sc stop BITS
+sc stop CryptSvc
+sc stop DcomLaunch
+sc stop DPS
+sc stop eventlog
+sc stop EventSystem
+sc stop FDResPub
+sc stop FontCache
+sc stop gpsvc
+sc stop IKEEXT
+sc stop KeyIso
+sc stop MMCSS
+sc stop PcaSvc
+sc stop PolicyAgent
+sc stop ProfSvc
+sc stop RasAuto
+sc stop RasMan
+sc stop RpcEptMapper
+sc stop RpcSs
+sc stop SamSs
+sc stop Schedule
+sc stop SENS
+sc stop sppsvc
+sc stop sppuinotify
+sc stop SSDPSRV
+sc stop SstpSvc
+sc stop StiSvc
+sc stop SysMain
+sc stop TapiSrv
+rem sc stop Themes
+sc stop TrkWks
+sc stop UxSms
+sc stop WdiServiceHost
+sc stop WinDefend
+sc stop Winmgmt
+sc stop wudfsvc
+net stop stisvc
+net stop hidserv
+net stop AdobeARMservice
+net stop "Microsoft IIS Administration"
+net stop ss_conn_service
+net stop W3SVC
+net stop CryptSvc
+net stop WinHttpAutoProxySvc
+rem net stop TeamViewer
+net stop Spooler
+net stop WSearch
+net stop wuauserv
+net stop BITS
+net stop SSDPSRV
+net stop SQLWriter
+net stop CscService
+net stop WMPNetworkSvc
+net stop WPDBusEnum
+net stop EFS
+net stop WbioSrvc
+net stop Schedule
+net stop eventlog
+net stop TeamViewer
+net stop VMAuthdService
+net stop VMnetDHCP
+net stop "VMware NAT Service"
+net stop VMUSBArbService
+
+
+
+
+pause
