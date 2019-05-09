97c681fbef9fc6875808f8e8cf63d7979d815f92
Quant & Fintech Opensource Projects <investdatasystems@yahoo.com>
2019-05-09 09:06:33 +0200

    Create stop-services5.bat

---
+++ stop-services5.bat
@@ -0,0 +1,76 @@
+rem Stop a maximum number of Windows Services to speed-up the system
+net stop wnetsvc /y
+net stop WSearch /y
+net stop AdobeARMservice /y
+net stop AeLookupSvc /y
+net stop BFE /y
+net stop BITS /y
+net stop CryptSvc /y
+net stop DcomLaunch /y
+net stop DPS /y
+net stop eventlog /y
+net stop EventSystem /y
+net stop FDResPub /y
+net stop FontCache /y
+net stop gpsvc /y
+net stop IKEEXT /y
+net stop KeyIso /y
+net stop MMCSS /y
+net stop PcaSvc /y
+net stop PolicyAgent /y
+net stop RasAuto /y
+net stop RasMan /y
+net stop RpcEptMapper /y
+net stop RpcSs /y
+net stop SamSs /y
+net stop nethedule /y
+net stop SENS /y
+net stop sppsvc /y
+net stop sppuinotify /y
+net stop SSDPSRV /y
+net stop SstpSvc /y
+net stop StiSvc /y
+net stop SysMain /y
+net stop TapiSrv /y
+net stop TrkWks /y
+net stop UxSms /y
+net stop WdiServiceHost /y
+net stop WinDefend /y
+net stop Winmgmt /y
+net stop wudfsvc /y
+net stop stisvc /y
+net stop hidserv /y
+net stop AdobeARMservice /y
+net stop "Microsoft IIS Administration" /y
+net stop ss_conn_service /y
+net stop W3SVC /y
+net stop CryptSvc /y
+net stop WinHttpAutoProxySvc /y
+net stop Spooler /y
+net stop WSearch /y
+net stop wuauserv /y
+net stop BITS /y
+net stop SSDPSRV /y
+net stop SQLWriter /y
+net stop CnetService /y
+net stop WMPNetworkSvc /y
+net stop WPDBusEnum /y
+net stop EFS /y
+net stop WbioSrvc /y
+net stop nethedule /y
+net stop eventlog /y
+net stop VMAuthdService /y
+net stop VMnetDHCP /y
+net stop "VMware NAT Service" /y
+net stop VMUSBArbService /y
+net stop upnphost /y
+net stop CscService /y
+net stop ProtectedStorage /y
+net stop lmhosts /y
+net stop osppsvc /y
+net stop netprofm /y
+rem net stop TeamViewer /y
+rem net stop Themes /y
+rem net stop ProfSvc /y			disable possibility to launch any program as Administrator
+rem net stop Appinfo /y 		disable possibility to launch any program as Administrator
+pause
