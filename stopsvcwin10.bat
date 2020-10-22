07b53feb2c8fa61b899194f6a263f974a6ac7317
Quant & Fintech Opensource Projects <reunisoft@gmail.com>
2020-10-22 10:04:30 +0200

    Add files via upload

---
+++ stopsvcwin10.bat
@@ -0,0 +1,32 @@
+net stop "wuauserv" /y
+net stop "TokenBroker" /y
+net stop "SstpSvc" /y
+net stop "SSDPSRV" /y
+net stop "RmSvc" /y
+net stop "hpqcaslwmiex" /y
+net stop "cphs" /y
+net stop "LanmanServer" /y
+net stop "igfxCUIService2.0.0.0" /y
+net stop "SgrmBroker" /y
+net stop "LMS" /y
+net stop "jhi_service" /y
+net stop "HPSupportSolutionsFrameworkService" /y
+net stop "HPJumpStartBridge" /y
+net stop "HP Comm Recover" /y
+net stop "WinDefend" /y
+net stop "W3SVC" /y
+net stop "VMware NAT Service" /y
+net stop "VMUSBArbService" /y
+net stop "VMnetDHCP" /y
+net stop "VMAuthdService" /y
+net stop "TeamViewer" /y
+net stop "stisvc" /y
+net stop "Intel(R) PROSet Monitoring Service" /y
+net stop "HotKeyServiceUWP" /y
+net stop "DusmSvc" /y
+net stop "DiagTrack" /y
+net stop "cplspcon" /y
+net stop "ClickToRunSvc" /y
+net stop "Bonjour Service" /y
+
+pause
