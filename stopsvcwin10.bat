2978ac80df8908b05b84238bdac900bc9226cfd3
Quant & Fintech Opensource Projects <reunisoft@gmail.com>
2020-10-28 09:46:19 +0100

    Add files via upload

---
+++ stopsvcwin10.bat
@@ -0,0 +1,50 @@
+net stop "wuauserv" /y
+net stop "TokenBroker" /y
+net stop "SstpSvc" /y
+net stop "SSDPSRV" /y
+net stop "RmSvc" /y
+net stop "hpqcaslwmiex" /y
+net stop "cphs" /y
+net stop "LanmanServer" /y
+net stop "igfxCUIService2.0.0.0" /y
+net stop "SgrmBroker" /y
+net stop "LMS" /y
+net stop "jhi_service" /y
+net stop "HPSupportSolutionsFrameworkService" /y
+net stop "HPJumpStartBridge" /y
+net stop "HP Comm Recover" /y
+net stop "WinDefend" /y
+net stop "W3SVC" /y
+net stop "VMware NAT Service" /y
+net stop "VMUSBArbService" /y
+net stop "VMnetDHCP" /y
+net stop "VMAuthdService" /y
+net stop "TeamViewer" /y
+net stop "stisvc" /y
+net stop "Intel(R) PROSet Monitoring Service" /y
+net stop "HotKeyServiceUWP" /y
+net stop "DusmSvc" /y
+net stop "DiagTrack" /y
+net stop "cplspcon" /y
+net stop "ClickToRunSvc" /y
+net stop "Bonjour Service" /y
+net stop "OneSyncSvc_1fdd5b" /y
+net stop "CDPUserSvc_1fdd5b" /y
+net stop "WpnService" /y
+net stop "wlidsvc" /y
+net stop "WinHttpAutoProxySvc" /y
+net stop "WpnUserService_1fdd5b" /y
+net stop "WpnUserService_1fdd5b" /y
+net stop "KeyIso" /y
+net stop "CertPropSvc" /y
+net stop "LicenseManager" /y
+net stop "DpHost" /y
+net stop "WbioSrvc" /y
+net stop "AppXSvc" /y
+net stop "RmSvc" /y
+net stop "ClipSVC" /y
+net stop "CryptSvc" /y
+net stop "Spooler" /y
+net stop "LanmanWorkstation" /y
+
+pause
